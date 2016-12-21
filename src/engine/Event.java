8c4edbc1d6f5caed73f3a9a9c40f2d37c6b85ffc
= <evilsnowflake>
2016-12-20 22:37:39 -0500

    added info to the event class

--- src/engine/Event.java
+++ src/engine/Event.java
@@ -2,5 +2,16 @@ package engine;
 
 public class Event {
 	int playerId;//who triggered the event or who's minion
-	String eventType; //single word description of what happened
+	Minion m;//minion that triggered event(such as summoned or activated effect)
+	Minion m2;//a minion that was affected by the event(such as got destroyed)
+	int mana;
+	String eventType; //description of what happened
+	/* types include
+	summon
+	attack
+	destroyed(this minion destroyed another)
+	wasDestroyed
+	spentMana
+	
+	*/
 }
