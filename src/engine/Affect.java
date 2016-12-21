ae0b9386891caff7f8273c784073800252e80d56
= <evilsnowflake>
2016-12-20 23:44:26 -0500

    what?

--- src/engine/Affect.java
+++
@@ -1,8 +0,0 @@
-package engine;
-//This is the super class for the large number of effects that will be created
-public abstract class Affect {
-	int effectId;
-	int triggerType;
-	Trigger trigger;//this is the object we ask to see if the affect will be activated
-	Effect affect;//this will change the game when activated
-}
