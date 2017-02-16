3e91ee326fe88f7b43411491d875e1a27ced8669
= <evilsnowflake>
2017-02-15 22:52:50 -0500

    start tests

---
+++ src/engine/UnitTestGameState.java
@@ -0,0 +1,14 @@
+package engine;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UnitTestGameState {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
