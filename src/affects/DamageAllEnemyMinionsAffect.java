04603ba61df4f856c21ae145b63dc7bdb09ae4bb
MaxMiller117 <MaxMiller117@gmail.com>
2017-03-02 13:14:48 -0500

    Assert statements for DamageAllEnemyMinionsAffect

--- src/affects/DamageAllEnemyMinionsAffect.java
+++ src/affects/DamageAllEnemyMinionsAffect.java
@@ -11,12 +11,15 @@ public class DamageAllEnemyMinionsAffect implements Affect{
 	
 	public DamageAllEnemyMinionsAffect(GameState gss, Minion ownerr, int damagee){
 		gs = gss;
+		assert gs != null;
 		damage = damagee;
 		owner = ownerr;
+		assert owner != null;
 	}
 	public void applyAffect() {
 		owner.effect.activationsThisTurn+=1;
 		Player enemy = gs.getEnemy(owner.owner);
+		assert enemy != null;
 		for(Minion m : enemy.minions){
 			DamageMinionActionAffect dmaa = new DamageMinionActionAffect(m, owner, damage);
 			gs.affectStack.addAction(dmaa);
